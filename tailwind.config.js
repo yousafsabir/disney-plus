7111dcfad9475fd99b14c44f30c05e6756ee700c
yousafsabir <yousafsabir430516@gmail.com>
2022-06-15 02:00:46 +0500

    colors,shadows and child selector added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'login-bg': "url('../src/assets/images/login-background.jpg')",
-        'home-bg': "url('../src/assets/images/home-background.png')",
-      },
-      colors: {
-        'login-btn': "#0063E4",
-        'body-bg': "#040417"
-      }
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "login-bg": "url('../src/assets/images/login-background.jpg')",
+                "home-bg": "url('../src/assets/images/home-background.png')",
+            },
+            colors: {
+                "login-btn": "#0063E4",
+                "body-bg": "#040417",
+                "viewer-border": "#f9f9f91a",
+            },
+            transitionTimingFunction: {
+                viewer: "cubic-bezier(0.25, 0.46, 0.45, 0.94)",
+            },
+            boxShadow: {
+                viewer: "0px 26px 20px -10px rgba(0, 0, 0, 0.69), 0px 16px 10px -10px rgba(0, 0, 0, 0.73)",
+                "viewer-hover":
+                    "0 40px 58px -16px rgba(0, 0, 0, 0.8), 0 30px 22px -10px rgba(0, 0, 0,  0.72 )",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [
+        function ({ addVariant }) {
+            addVariant("video-hover", "&:hover > video");
+        },
+    ],
+};
