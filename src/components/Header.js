07eee16902a3a6a9c4a871374a45fa2dfdb25383
yousafsabir <yousafsabir430516@gmail.com>
2022-06-14 13:25:57 +0500

    header, home and login added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='w-full min-h-screen flex items-center justify-center'>
+            <div className="content">
+            </div>
+        </header>
+    )
+}
+
+export default Header
