723812d301fc19ad88f3279db6bb688bfc07def2
yousafsabir <yousafsabir430516@gmail.com>
2022-06-15 19:59:19 +0500

    viewer component

--- src/components/ViewerWrap.js
+++ src/components/ViewerWrap.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ViewerWrap = (props) => {
     return (
-        <div className="video relative cursor-pointer overflow-hidden rounded-xl border-[3px]  border-viewer-border pt-[55.25%] shadow-viewer transition-all duration-300 ease-viewer hover:scale-105 hover:shadow-viewer-hover video-hover:opacity-100">
+        <div className="relative cursor-pointer overflow-hidden rounded-xl border-[3px]  border-viewer-border pt-[55.25%] shadow-viewer transition-all duration-300 ease-viewer hover:scale-105 hover:shadow-viewer-hover video-hover:opacity-100">
             <img
                 className="absolute top-0 z-10 transition-all duration-500  ease-in-out hover:opacity-0"
                 src={props.img}
